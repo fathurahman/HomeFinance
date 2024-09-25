5dff07b2a8e024ce29711f791e6f1a92b4f9ac5a
fathurahman <ipat.bogor@gmail.com>
2024-09-25 14:16:04 +0700

    add 'file' actions and menubar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,48 +1,98 @@
 ﻿#include "mainwindow.h"
 #include "database.h"
 #include <QDebug>
-
+#include <QSettings>
+#include <QFileDialog>
+#include <QApplication>
+#include <QMenuBar>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    auto* db = new Database(this);
-    /*
-    int wi = db->addWallet("cash", 0);
-    auto& w = db->wallet(wi);
-    Journal j;
-    j.timestamp = QDateTime::currentDateTime();
-    j.title = "test";
-    j.isDebit = true;
-    j.addEntry("bca", 1, 1500000);
-    db->addJournal(w.index, j);
-    j.title = "belanja";
-    j.entries.clear();
-    j.isDebit = false;
-    j.addEntry("gorengan", 4, 5000);
-    j.addEntry("sampeu", 1, 32500);
-    j.addEntry("sania", 1, 18250);
-    db->addJournal(w.index, j);
-    w.debug();
-    db->save("D:/ucup.json");
-    */
-    db->load("D:/ucup.json");
-    auto& w = db->wallet(0);
-    Journal j;
-    j.title = "belanja 2";
-    j.timestamp = QDateTime::currentDateTime();
-    j.entries.clear();
-    j.isDebit = false;
-    j.addEntry("gorengan", 10, 10000);
-    j.addEntry("bimoli", 2, 13000);
-    j.addEntry("rtx 4090", 1, 350000);
-    db->addJournal(w.index, j);
-    w.debug();
-    qDebug() << db->journalEntryNames();
-
+    m_db = new Database(this);
+    createActions();
+    createMenus();
 }
 
 MainWindow::~MainWindow()
 {
+}
+
+void MainWindow::open()
+{
+    QSettings s;
+    QString filePath = s.value("LastFilePath", "").toString();
+    filePath = QFileDialog::getOpenFileName(this, "Open Database File", filePath, "JSON (*.json)");
+    if (!filePath.isEmpty())
+    {
+        s.setValue("LastFilePath", filePath);
+        m_db->load(filePath);
+        m_actSave->setEnabled(true);
+        m_actSaveAs->setEnabled(true);
+    }
+}
+
+void MainWindow::save()
+{
+    QSettings s;
+    QString filePath = s.value("LastFilePath", "").toString();
+    if (filePath.isEmpty())
+    {
+        filePath = QFileDialog::getSaveFileName(this, "Save Database File", "", "JSON (*.json)");
+        if (filePath.isEmpty())
+            return;
+    }
+    m_db->save(filePath);
+}
+
+void MainWindow::saveAs()
+{
+    QSettings s;
+    QString filePath = s.value("LastFilePath", "").toString();
+    filePath = QFileDialog::getSaveFileName(this, "Save Database File As", filePath, "JSON (*.json)");
+    if (!filePath.isEmpty())
+    {
+        s.setValue("LastFilePath", filePath);
+        m_db->save(filePath);
+    }
+}
+
+void MainWindow::quit()
+{
+    qApp->quit();
+}
+
+void MainWindow::createActions()
+{
+    m_actOpen = new QAction("&Open", this);
+    m_actOpen->setShortcuts(QKeySequence::Open);
+    m_actOpen->setStatusTip("Open database file");
+    connect(m_actOpen, &QAction::triggered, this, &MainWindow::open);
+
+    m_actSave = new QAction("&Save", this);
+    m_actSave->setShortcuts(QKeySequence::Save);
+    m_actSave->setStatusTip("Save database file");
+    connect(m_actSave, &QAction::triggered, this, &MainWindow::save);
+
+    m_actSaveAs = new QAction("Save &As", this);
+    m_actSaveAs->setShortcuts(QKeySequence::SaveAs);
+    m_actSaveAs->setStatusTip("Save database file as");
+    connect(m_actSaveAs, &QAction::triggered, this, &MainWindow::saveAs);
+
+    m_actQuit = new QAction("&Quit", this);
+    m_actQuit->setShortcuts(QKeySequence::Quit);
+    m_actQuit->setStatusTip("Quit Home Finance");
+    connect(m_actQuit, &QAction::triggered, this, &MainWindow::quit);
+}
+
+void MainWindow::createMenus()
+{
+    auto mb = menuBar();
+    m_fileMenu = mb->addMenu("&File");
+    m_fileMenu->addAction(m_actOpen);
+    m_fileMenu->addAction(m_actSave);
+    m_fileMenu->addAction(m_actSaveAs);
+    m_fileMenu->addSeparator();
+    m_fileMenu->addAction(m_actQuit);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAction>
+#include "database.h"
 
 class MainWindow : public QMainWindow
 {
@@ -10,5 +12,27 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    Database *m_db;
+
+    QAction* m_actOpen;
+    QAction* m_actSave;
+    QAction* m_actSaveAs;
+    QAction* m_actQuit;
+
+    QMenu* m_fileMenu;
+
+private slots:
+    void open();
+    void save();
+    void saveAs();
+    void quit();
+
+private:
+    void createActions();
+    void createMenus();
+
+
 };
 #endif // MAINWINDOW_H
